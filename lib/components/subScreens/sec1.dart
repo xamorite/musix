12c8ef8f932713427ff96e3147fc1e7887c9d598
Ogunneye Emmanuel <eogunneye@gmail.com>
2024-06-03 21:46:53 +0100

    featauring events

--- lib/components/subScreens/sec1.dart
+++ lib/components/subScreens/sec1.dart
@@ -17,67 +17,209 @@ class _Featuring_TodayState extends State<Featuring_Today> {
         children: [
           Text(
             'Featuring Today',
-            style: TextStyle(color: Colors.white, fontSize: 30),
-          ),
-          SizedBox(
-            height: 30,
+            style: TextStyle(color: Colors.white, fontSize: 24),
           ),
           Container(
-            height: 200,
-            width: double.infinity,
+            height: 180,
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(20),
               // color: Colors.white,
             ),
             child: LayoutBuilder(builder: (context, constraints) {
-              return SingleChildScrollView(
-                primary: false,
-                child: ConstrainedBox(
-                  constraints: BoxConstraints(minHeight: constraints.maxHeight),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Stack(
-                        // alignment: Alignment.centerLeft,
-                        children: [
-                          Image(
-                              image: AssetImage(
-                                'assets/images/download.jpg',
-                              ),
-                              fit: BoxFit.contain),
-                          Column(
-                            children: [
-                              Text(
-                                'Agallio',
-                                style: TextStyle(
-                                    fontSize: 35, color: Colors.white),
-                              )
-                            ],
+              return ListView(
+                scrollDirection: Axis.horizontal,
+                children: [
+                  Container(
+                    width: 300,
+                    height: 200,
+                    alignment: Alignment.bottomLeft,
+                    padding: EdgeInsets.symmetric(horizontal: 20),
+                    margin: EdgeInsets.all(5),
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage(
+                            'assets/images/english.png',
                           ),
-                        ],
-                      ),
-                      Stack(
-                        // alignment: Alignment.centerLeft,
-                        children: [
-                          Image(
-                              image: AssetImage(
-                                'assets/images/download.jpg',
+                          fit: BoxFit.fill),
+                    ),
+                    child: Align(
+                      alignment: Alignment.bottomLeft,
+                      child: Padding(
+                        padding: EdgeInsets.only(bottom: 20),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Text(
+                              'New',
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
                               ),
-                              fit: BoxFit.contain),
-                          Column(
-                            children: [
-                              Text(
-                                'Agallio',
-                                style: TextStyle(
-                                    fontSize: 35, color: Colors.white),
-                              )
-                            ],
-                          ),
-                        ],
+                            ),
+                            Text(
+                              'ENGLISH\nSONGS',
+                              style: TextStyle(
+                                  fontSize: 24,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ],
+                        ),
                       ),
-                    ],
+                    ),
                   ),
-                ),
+                  Container(
+                    width: 300,
+                    padding: EdgeInsets.symmetric(horizontal: 20),
+                    margin: EdgeInsets.all(5),
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage(
+                            'assets/images/weekly.png',
+                          ),
+                          fit: BoxFit.fill),
+                    ),
+                    child: Padding(
+                      padding: EdgeInsets.only(bottom: 20),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          Text(
+                            'Weekly',
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
+                          ),
+                          Text(
+                            'Top 20',
+                            style: TextStyle(
+                                fontSize: 24,
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: 300,
+                    padding: EdgeInsets.symmetric(horizontal: 20),
+                    margin: EdgeInsets.all(5),
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage(
+                            'assets/images/sing.png',
+                          ),
+                          fit: BoxFit.fill),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.only(bottom: 20),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          Text(
+                            'Sing along with',
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
+                          ),
+                          Text(
+                            'Tye Tribbet',
+                            style: TextStyle(
+                                fontSize: 24,
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: 300,
+                    padding: EdgeInsets.symmetric(horizontal: 20),
+                    margin: EdgeInsets.all(5),
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage(
+                            'assets/images/allnew.png',
+                          ),
+                          fit: BoxFit.fill),
+                    ),
+                    child: Padding(
+                      padding: EdgeInsets.only(bottom: 20),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          Text(
+                            'All New from',
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
+                          ),
+                          Text(
+                            'TAMIL\nTRENDING',
+                            style: TextStyle(
+                                fontSize: 24,
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: 300,
+                    height: 200,
+                    alignment: Alignment.bottomLeft,
+                    padding: EdgeInsets.symmetric(horizontal: 20),
+                    margin: EdgeInsets.all(5),
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage(
+                            'assets/images/thisweek.png',
+                          ),
+                          fit: BoxFit.fill),
+                    ),
+                    child: Align(
+                      alignment: Alignment.bottomLeft,
+                      child: Padding(
+                        padding: EdgeInsets.only(bottom: 20),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Text(
+                              'This Weeks',
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                              ),
+                            ),
+                            Text(
+                              'AFRO\nGOSPEL',
+                              style: TextStyle(
+                                  fontSize: 24,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               );
             }),
           ),
